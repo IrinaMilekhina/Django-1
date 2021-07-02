f3d2aea6ef6f080c89bf5c795103ba81047c46de
IrinaMilekhina <i.mezhova@gmail.com>
2021-07-02 11:31:35 +0300

    переопредение методов __str__

--- products/models.py
+++ products/models.py
@@ -8,16 +8,22 @@ from djmoney.models.fields import MoneyField
 
 class ProductCategory(models.Model):
     name = models.CharField(max_length=64, unique=True)
-    description = models.TextField(blank=True,
-                                   null=True)  # если не стоит null, то поле по прежнему обязательно к заполнению, но будет заполняться пустой строкой
+    # если не стоит null, то поле по прежнему обязательно к заполнению, но будет заполняться пустой строкой
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
 
 
 class Product(models.Model):
     name = models.CharField(max_length=256)
-    image = models.ImageField(upload_to='products_images',
-                              blank=True)  # папка products_images создается автоматически после миграций
+    # папка products_images создается автоматически после миграций
+    image = models.ImageField(upload_to='products_images', blank=True)
     description = models.TextField(blank=True, null=True)
-    price = MoneyField(decimal_places=2, default=0, default_currency='RUB',
-                       max_digits=11)  # библиотека https://github.com/django-money/django-money
+    # библиотека https://github.com/django-money/django-money
+    price = MoneyField(decimal_places=2, default=0, default_currency='RUB', max_digits=11)
     quantity = models.PositiveIntegerField(default=0)
     category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)  # on_delete=models.CASCADE/SET_NULL/PROTECT
+
+    def __str__(self):
+        return f'{self.name} | {self.category.name}'
