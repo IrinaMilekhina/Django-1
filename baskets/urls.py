d456d1ffff5e5cc78302567e94090a11e8254714
IrinaMilekhina <i.mezhova@gmail.com>
2021-07-08 13:09:20 +0300

    добавления метода sum() для basket

--- baskets/urls.py
+++ baskets/urls.py
@@ -5,5 +5,5 @@ from baskets.views import baskets_add, basket_remove
 app_name = 'baskets'
 urlpatterns = [
     path('add/<int:product_id>', baskets_add, name='baskets_add'),
-path('remove/<int:id>', basket_remove, name='basket_remove'),
+    path('remove/<int:id>', basket_remove, name='basket_remove')
 ]
